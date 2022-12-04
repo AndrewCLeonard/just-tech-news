054ddbd7518ff1d44f80105215f2044b2ca664db
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-12-03 22:00:01 -0800

    login/logout functionality created

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,5 +1,3 @@
-// const { response } = require("express");
-
 async function logout() {
 	const response = await fetch("/api/users/logout", {
 		method: "post",
@@ -7,8 +5,10 @@ async function logout() {
 	});
 
 	if (response.ok) {
+		console.log(`\nresponse.ok\n`);
 		document.location.replace("/");
 	} else {
+		console.log(`\nresponse NOT ok\n`);
 		alert(response.statusText);
 	}
 }
